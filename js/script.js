e36c3cec63615f1505e9239a83124432d8d9df54
emily <100404012@nccu.edu.tw>
2014-06-18 17:43:58 +0800

    push

--- js/script.js
+++ js/script.js
@@ -54,11 +54,11 @@ FB.getLoginStatus(function (response) {
 	ctx.font='20px "Arial"';//設定字體與大小
 	ctx.fillText("Click here to start fill with Facebook Profile Picture", 40, 270); //設定預設的開始畫面
     var img = new Image(); // 新增圖像1
-    img.src = "img/img_1.jpg"; //圖像路徑（路徑自己設，且自己加入想要的圖層）
+    img.src = "img/01.jpg"; //圖像路徑（路徑自己設，且自己加入想要的圖層）
 	var img2 = new Image(); //新增圖像2
-	img2.src = "img/img_2.jpg" //圖像路徑
+	img2.src = "img/02.jpg" //圖像路徑
 	var img3 = new Image();//新增圖像3
-	img3.src = "img/img_3.jpg"//圖像路徑
+	img3.src = "img/03.jpg"//圖像路徑
 	
 
 	//宣告基本變數
